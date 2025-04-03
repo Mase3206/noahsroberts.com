f8de008536442d53aca4c24bf3324f3afe1bf372
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2025-04-02 22:34:25 -0600

    pretty similar homepage

---
+++ src/lib/components/DotSeparator.svelte
@@ -0,0 +1,13 @@
+<!-- literally just copied from Wordpress -->
+<style lang="scss">
+	div {
+		font-size: 1.5em;
+		letter-spacing: 2em;
+		margin-left: auto;
+		margin-right: auto;
+		padding-left: 2em;
+		text-align: center;
+	}
+</style>
+
+<div>...</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import DotSeparator from "$lib/components/DotSeparator.svelte";
+	import PostList from "$lib/components/PostList.svelte";
+
+	export let data;
+	const { posts } = data;
+
+	// let posts: { meta: any, path: string }[] = [];
+
+	// // modifies `posts` directly
+	// async function fetchPosts() {
+	// 	const response = await fetch('/api/posts');
+	// 	posts = await response.json();
+	// }
+	
+	// fetchPosts();
+</script>
+
+<h1>Welcome.</h1>
+<img src="https://noahsroberts.com/wp-content/uploads/2023/10/headshot-1-edited-3.jpg?w=2046" alt="" />
+
+<p>
+	Hey there! I am an Eagle Scout and Computer Science major with a minor in German at the University of Montana. I am a member of the Davidson Honors College and the Franke Global Leadership Initiative. I enjoy music, theater, transit design, being outside, and learning about the world.
+</p>
+
+<DotSeparator />
+
+<h2>Recent updates</h2>
+
+<PostList {posts} />
