8f8e4988a039ca21c13030e6010b1242a1e3de1a
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2025-04-08 14:41:28 -0600

    add some dark mode

--- src/lib/components/portfolio/Show.svelte
+++ src/lib/components/portfolio/Show.svelte
@@ -38,7 +38,7 @@
 {/snippet}
 
 
-<div class="bg-gray-100 p-8 mb-8 overflow-visible show-card w-{width * 4}" style="left: {-((width) / 4) + 1.5}em">
+<div class="bg-neutral-100 dark:bg-neutral-800 p-8 mb-8 overflow-visible show-card w-{width * 4}" style="left: {-((width) / 4) + 1.5}em">
 	<div>
 		<h2 class="italic mb-0 mt-0">{title}.</h2>
 		<p class="mt-0 mb-2">by {author}</p>
