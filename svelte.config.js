a980b09c968f8ba5c7e45618529872797acaca29
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2025-04-15 14:35:09 -0600

    add github callouts, cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -2,17 +2,23 @@ import { mdsvex } from 'mdsvex';
 // import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import remarkGithubAlerts from 'remark-github-alerts';
+// import remarkParse from 'remark-parse';
+// import remarkFrontmatter from 'remark-frontmatter';
+// import remarkDirective from 'remark-directive';
+import rehypeCallouts from 'rehype-callouts';
 
 const config = {
 	preprocess: [
 		// vitePreprocess(),  // sass preprocessor/
 		mdsvex({
 			extensions: ['.md'],
+			rehypePlugins: [ rehypeCallouts ],
 			layout: {
 				// no layout for posts, as those are handled dynamically
 				
 				normal_page: 'src/lib/layouts/normal_page.svelte',
-			}
+			},
 		})
 	],
 	kit: { adapter: adapter() },
