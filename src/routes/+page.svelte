d8054160468caf14dc049aea762a469545ff49d3
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2025-04-16 21:09:27 -0600

    import assets from wordpress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <h1>Welcome.</h1>
-<img src="https://noahsroberts.com/wp-content/uploads/2023/10/headshot-1-edited-3.jpg?w=2046" alt="" />
+<img src="/assets/headshot.jpg" alt="" />
 
 <p>
 	Hey there! I am an Eagle Scout and Computer Science major with a minor in German at the University of Montana. I am a member of the Davidson Honors College and the Franke Global Leadership Initiative. I enjoy music, theater, transit design, being outside, and learning about the world.
